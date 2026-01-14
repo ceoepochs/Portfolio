a24f3a2badaec684493d5edaa83f55a0510be427
Colton Osterlund <36303572+ColtonOsterlund@users.noreply.github.com>
2026-01-14 11:47:32 -0700

    Changing hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -184,7 +184,7 @@ export default function Hero() {
                     transition: 'opacity 0.2s linear',
                   }}
                 >
-                  Hi, my name is Colton
+                  Hi, my name is Colton.
                 </Typography>
                 <Typography
                   variant="h3"
@@ -195,7 +195,7 @@ export default function Hero() {
                     transition: 'opacity 0.2s linear',
                   }}
                 >
-                  I'm a full-stack software engineer who has written and deployed production level code for clients in 10+ different industries
+                  I'm a full-stack software engineer who has written and deployed production level code for clients in 10+ different industries.
                 </Typography>
                 <Typography
                   variant="h3"
@@ -206,7 +206,7 @@ export default function Hero() {
                     transition: 'opacity 0.2s linear',
                   }}
                 >
-                  I take pride in my curiosity and I'm passionate about continually developing my skills and learning new technologies
+                  I'm passionate about continually developing my skills and learning new technologies.
                 </Typography>
                 <Typography
                   variant="h3"
@@ -217,7 +217,7 @@ export default function Hero() {
                     transition: 'opacity 0.2s linear',
                   }}
                 >
-                  Welcome to my portfolio
+                  Welcome.
                 </Typography>
               </Box>
             </Box>
